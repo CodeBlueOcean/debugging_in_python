13b1dd2cff7709d048eaa4bfc022b872fc8da45e
CodeBlueOcean <95150255+CodeBlueOcean@users.noreply.github.com>
2022-03-26 23:59:17 -0500

    Add files via upload

---
+++ debugging.py
@@ -0,0 +1,21 @@
+# debugging
+# linting 
+# ide/ editor 
+# read errors
+4 + 'kldjdalkjd'
+
+'name' + 4 
+
+# pdb
+import pdb
+
+def add(num1, num2):
+    pdb.set_trace()
+    t = 4*5
+    print(num1, num2)
+    return num1 + num2
+
+add(4, 'hhkhads') 
+
+
+
---
+++ script.py
@@ -0,0 +1,11 @@
+my_file = open('test.txt')
+
+print(my_file.read())
+my_file.seek(0)
+print(my_file.read())
+my_file.seek(0)
+print(my_file.read())
+
+print(my_file.readline())
+
+my_file.close()
